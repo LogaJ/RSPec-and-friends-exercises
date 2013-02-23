e7a23078060c301d4cd7e09d30194c703f0d9b5a
LogiBogi <loga.jegede+github@gmail.com>
2013-02-23 20:37:34 +0000

    SRP for Marker class completed.

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,35 +11,35 @@ module Codebreaker
     end
 
     def guess(guess)
-      marker = Marker.new(@secret)
-      @output.puts '+'*marker.exact_match_count(guess) + 
-                   '-'*marker.number_match_count(guess)
+      marker = Marker.new(@secret, guess)
+      @output.puts '+'*marker.exact_match_count + 
+                   '-'*marker.number_match_count
     end
   end
 
   class Marker
-    def initialize(secret)
-      @secret = secret
+    def initialize(secret, guess)
+      @secret, @guess = secret, guess
     end
 
-    def exact_match_count(guess)
+    def exact_match_count
       (0..3).inject(0) do |count, index|
-        count + (exact_match?(guess, index) ? 1 : 0)
+        count + (exact_match?(index) ? 1 : 0)
       end
     end
 
-    def number_match_count(guess)
+    def number_match_count
       (0..3).inject(0) do |count, index|
-        count + (number_match?(guess, index) ? 1 : 0)
+        count + (number_match?(index) ? 1 : 0)
       end
     end
 
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
+    def exact_match?(index)
+      @guess[index] == @secret[index]
     end
 
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
+    def number_match?(index)
+      @secret.include?(@guess[index]) && !exact_match?(index)
     end
   end
 end

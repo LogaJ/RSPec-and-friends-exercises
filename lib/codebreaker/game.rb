ef4c7c8d23d0c0be5321b04c81c288cf8fa22d3c
LogiBogi <loga.jegede+github@gmail.com>
2013-02-23 19:57:59 +0000

    codesmell exercise: extract number match to method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,13 +11,7 @@ module Codebreaker
     end
 
     def guess(guess)
-      number_match_count = 0
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
-      @output.puts '+'*exact_matches_count(guess) + '-'*number_match_count
+      @output.puts '+'*exact_matches_count(guess) + '-'*number_match_count(guess)
     end
 
     def exact_matches_count(guess)
@@ -28,6 +22,16 @@ module Codebreaker
         end
       end
       exact_matches_count
+    end
+
+    def number_match_count(guess)
+      number_match_count = 0
+      (0..3).each do |index|
+        if number_match?(guess, index)
+          number_match_count += 1
+        end
+      end
+      number_match_count
     end
 
     def exact_match?(guess, index)

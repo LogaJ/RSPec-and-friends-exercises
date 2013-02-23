d5088698e7da058a5b1758d6643d4c14d1b23d84
LogiBogi <loga.jegede+github@gmail.com>
2013-02-23 16:34:21 +0000

    adding a secret code to begin guesses.

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -15,9 +15,14 @@ end
 Given /^I am not yet playing$/ do
 end
 
+Given /^the secret code is "(.*?)"$/ do |secret|
+	@game = Codebreaker::Game.new(output)
+	@game.start(secret)
+end
+
 When /^I start a new game$/ do
 	game = Codebreaker::Game.new(output)
-	game.start
+	game.start('1234')
 end
 
 Then /^I should see "(.*?)"$/ do |message|
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -7,11 +7,11 @@ module Codebreaker
 			let(:game) 	 { Game.new(output) }
 			it "sends a welcome message" do
 				output.should_receive(:puts).with('Welcome to Codebreaker!')
-				game.start
+				game.start('1234')
 			end
 			it "prompts for the first guess" do
 				output.should_receive(:puts).with('Enter guess:')
-				game.start
+				game.start('1234')
 			end
 		end
 	end
